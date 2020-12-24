4037b3ce67eeac25df8f04d60dd9389529684f1c
luxin433 <luxin433@gmail.com>
2020-12-24 19:53:14 +0800

    update bot logic.

--- bots/114bot/dialog_config/functions.py
+++ bots/114bot/dialog_config/functions.py
@@ -1,17 +1,23 @@
 import logging
 from types import SimpleNamespace
 from .callwatcher.client import CallManager
+from pypinyin import lazy_pinyin
 ENV="dev"
 
 if ENV == "production":
     from .api.soap_api import query
 else:
     from .api.json_api import query
-
+    
 ## order_time
 def query_phone_by_car_no(user_utter, global_vars, context = None):
     ENDPOINT="users"
     logging.info(f"query_phone_by_car_no: user_utter={user_utter}, context={context}")
+    try: 
+        if lazy_pinyin(car_no[0])[0][0:3] != "jin":
+            return False
+    except:
+        pass
     data = query(ENDPOINT, car_no=global_vars['car_no'])
     if not data:
         return False
@@ -34,11 +40,25 @@ def call_manager_process(user_utter, global_vars, context = None):
     # Tricky地用global_vars来返回结果
     global_vars['func_message'] = msg
     return code
+    
+def test_tianjin_chepai(car_no):
+    try: 
+        if lazy_pinyin(car_no[0])[0][0:3] != "jin":
+            print(f"{car_no} 非天津车牌")
+        else:
+            print(f"{car_no} 天津车牌")
+    except:
+        pass
+    
 
 if __name__ == "__main__":
     car_no = "津A12345"
-    res = query(f'users',car_no=car_no)
+    #res = query(f'users',car_no=car_no)
+    #print(res)
     global_vars = {'car_no': car_no}
     p_res = query_phone_by_car_no({},global_vars)
-    print("query result:", res)
-    print(p_res, global_vars)
+    print("query result:", p_res)
+    # print(p_res, global_vars)
+    car_no_list = ["津A12345", "金123455","晋123456","京123456","冀123456","东","南"]
+    for car_no in car_no_list:
+        test_tianjin_chepai(car_no)
